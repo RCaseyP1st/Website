b0f1e83a5942c4fcec3ab57e95b2541a2e572409
RCaseyP1st <Richard@Parents1st.org.uk>
2025-05-08 10:31:02 +0100

    Auto-commit before deploy

--- src/components/Dashboard/DashboardSectionWrapper.js
+++ src/components/Dashboard/DashboardSectionWrapper.js
@@ -1,10 +1,10 @@
-// DashboardSectionWrapper.jsx
-const DashboardSectionWrapper = ({ children }) => {
-  return (
-    <div className="max-w-screen-xl mx-auto space-y-12 px-6 pt-6">
-      {children}
+const DashboardSectionWrapper = ({ children }) => (
+  <section className="mb-12 w-full">
+    <div className="w-full text-center">
+      {/* Title should be in the component, but alignment is enforced here */}
     </div>
-  );
-};
+    {children}
+  </section>
+);
 
 export default DashboardSectionWrapper;
